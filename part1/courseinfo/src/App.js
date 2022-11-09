2919260c45c3894b7ffb414db176da70f87fb898
jaidensy07 <jaidensy07@gmail.com>
2022-11-08 17:07:42 -0800

    refactored app variables

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,42 +1,51 @@
 const Header = (props) => {
-  <>
-    <h1>{props.course}</h1>
-  </>
+  console.log(props)
+  return <h1>{props.course}</h1>
 }
 
 const Content = (props) => {
+  return (
   <>
     <p>
-      {props.part1} {props.exercises1}
+      {props.part1} 
     </p>
     <p>
-      {props.part2} {props.exercises2}
+      {props.part2} 
     </p>
     <p>
-      {props.part3} {props.exercises3}
+      {props.part3} 
     </p>
   </>
-  
+  )
 }
 const Total = (props) => {
+  return (
   <>
   <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
   </>
+  )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development', 
+    parts: [
+    {
+      name: 'Fundamentals of React', excercises: 10
+    }, 
+    {
+      name: 'Using props to pass data', excercises: 7
+    }, 
+    {
+      name: 'State of a component', excercises: 14
+    }
+  ]}
+
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Header course={course.name}/>
+      <Content part1={course.parts[0]} part2={course.parts[1]} part3={course.parts[2]}/>
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
     </div>
   )
